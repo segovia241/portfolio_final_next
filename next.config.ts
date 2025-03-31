c5647546249cc32c8f0e87ce2b185ea7fb64780a
segovia241 <segovia.network@gmail.com>
2025-03-31 15:54:52 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //output: 'export'
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  images: {
+    domains: ['inkscape.app'], // Añadimos el dominio de la imagen
+  },
 };
 
 export default nextConfig;
